1d124d17a163d874a2e403e65d704bb29cf061bc
liamoar <avishr44@gmail.com>
2018-11-28 01:50:11 +0545

    changes in mailer file

--- mailer.php
+++ mailer.php
@@ -28,7 +28,7 @@ function clean_text($string)
       window.location.href = 'http://localhost:8080/ContactUs.html';
     </script>";
  }
- 
+
  elseif(empty($_POST["email"]))
  {
   echo "<script>alert('You should provide an Email Id.')</script>
@@ -37,7 +37,7 @@ function clean_text($string)
     </script>";
  }
  elseif ($_POST["email"] != filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  
+
    echo "<script>alert('You should valid Email Id.')</script>
     <script>
       window.location.href = 'http://localhost:8080/ContactUs.html';
@@ -58,12 +58,12 @@ function clean_text($string)
   $mail->Host = "smtp.gmail.com";  //Sets the SMTP hosts
   $mail->Port = "465";        //Sets the default SMTP server port
   $mail->SMTPAuth = true;       //Sets SMTP authentication. Utilizes the Username and Password variables
-  $mail->Username = "kessi17sabin@gmail.com";     //Sets SMTP username
-  $mail->Password = "9813923416";     //Sets SMTP password
+  $mail->Username = "";     //Sets SMTP username  recipent gmailmail id
+  $mail->Password = "";     //Sets SMTP password  recipent actual gmail password
   $mail->SMTPSecure = "ssl";       //Sets connection prefix. Options are "", "ssl" or "tls"
   $mail->From = $email;     //Sets the From email address for the message
   $mail->FromName = $name;    //Sets the From name of the message
-  $mail->AddAddress("kessi1807sabin@gmail.com");//Adds a "To" address
+  $mail->AddAddress("avishr75@gmail.com");//Adds a "To" address
   $mail->AddCC = $email;
   $mail->IsHTML(true);       //Sets message type to HTML
   $mail->Subject = "Enquiry from websites submitted by $name";    //Sets the Subject of the message
@@ -96,9 +96,9 @@ function clean_text($string)
     echo "<script>alert('Your message has been sent successfully. We will get back to you soon. Thanks for contacting us.')</script>
     <script>
       window.location.href = 'http://test.sslfreight.com.np/ContactUs.html';
-    </script>";   
+    </script>";
   }
-  } 
-  }        
-  
+  }
+  }
+
 ?>
